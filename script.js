1af5840304fa3adda52567d4a9d22a992f85357b
Aqib904 <74586324+Aqib904@users.noreply.github.com>
2020-11-17 13:22:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+function display(){
+    document.getElementById("img1").style.display="block";
+    document.getElementById("btnfr").style.backgroundColor="white";
+}
+function block(){
+    document.getElementById("img1").style.display="none";
+    document.getElementById("btnfr").style.backgroundColor="grey";
+}
+ function show(){
+     document.getElementById("one").style.display="inline-block"; 
+     document.getElementById("two").style.display="inline-block"; 
+     document.getElementById("three").style.display="inline-block"; 
+     document.getElementById("four").style.display="inline-block"; 
+     document.getElementById("five").style.display="inline-block"; 
+     document.getElementById("six").style.display="inline-block"; 
+     document.getElementById("seven").style.display="inline-block"; 
+     document.getElementById("eight").style.display="inline-block"; 
+     document.getElementById("nine").style.display="inline-block";             
+ }
+ function print(){
+     document.getElementById("dis").style.display="block";
+ }
+ function disp(result){
+     form.textarea.value =   form.textarea.value + result;
+ }
+function next(){
+let p0=   document.getElementById("one");
+  document.getElementById("two");
+  document.getElementById("three");
+  document.getElementById("four");
+ let a='A';
+  if(p0=!a);
+  {         document.getElementById("press8").style.backgroundColor="red";
+      }
+}
